626a48e19a3c8e313b343b5b26b703330b2d5547
Shant <shant.asf@gmail.com>
2021-02-22 22:33:05 +0400

    fix Object type

--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -12,8 +12,8 @@ import FileUpload from '../../components/FileUpload';
 
 const Create: React.FC = () => {
   const [activeStep, setActiveStep] = useState<number>(0);
-  const [picture, setPicture] = useState<Object | null>(null);
-  const [audio, setAudio] = useState<Object | null>(null);
+  const [picture, setPicture] = useState<Object<{ name: string }> | null>(null);
+  const [audio, setAudio] = useState<Object<{ name: string }> | null>(null);
   const name = useInput('');
   const artist = useInput('');
   const text = useInput('');
