620bffd2b536f526c6aeb5ccda2ebc5bd81f8ea7
Shant <shant.asf@gmail.com>
2021-02-22 23:27:42 +0400

    fix

--- store/actions/track.ts
+++ store/actions/track.ts
@@ -1,14 +1,10 @@
-import { AnyAction, Action, ActionCreator, Dispatch } from 'redux';
-import { ThunkAction } from 'redux-thunk';
 import axios from 'axios';
 
 import { TrackAction, TrackActionTypes } from '../../types/track';
 
 const apiHost = process.env.API_HOST;
 
-export const fetchTracks: ActionCreator<
-  ThunkAction<Promise<Action>, TrackAction, void, AnyAction>
-> = () => async (dispatch: Dispatch<TrackAction>): Promise<Action> => {
+export const fetchTracks = () => async (dispatch: Dispatch<TrackAction>) => {
   try {
     const response = await axios.get(`${apiHost}/tracks`);
     dispatch({ type: TrackActionTypes.FETCH_TRACKS, payload: response.data });
@@ -20,11 +16,9 @@ export const fetchTracks: ActionCreator<
   }
 };
 
-export const searchTracks: ActionCreator<
-  ThunkAction<Action, RootState, void, AnyAction>
-> = (query: string) => async (
+export const searchTracks = (query: string) => async (
   dispatch: Dispatch<TrackAction>
-): Promise<Action> => {
+) => {
   try {
     const response = await axios.get(`${apiHost}/tracks/search?query=${query}`);
     dispatch({ type: TrackActionTypes.FETCH_TRACKS, payload: response.data });
